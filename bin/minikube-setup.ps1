9865d34517bc9d540330421af2fe3f8659ef2711
Marfien <contact@marfien.dev>
2024-11-22 17:16:55 +0100

    Add continues running process with cleanup

--- bin/minikube-setup.ps1
+++ bin/minikube-setup.ps1
@@ -18,3 +18,17 @@ Get-DnsClientNrptRule | Where-Object { $_.Namespace -eq "food2gether.local" } | 
 # Add new DNS rule
 Add-DnsClientNrptRule -Namespace "food2gether.local" -NameServer "$(minikube ip)"
 
+if ($ApplicationComponent -ne $null) {
+  flux suspend $ApplicationComponent -n food2gether
+  kubectl delete -k .\deployment\prod
+  kubectl apply -k .\deployment\local
+}
+
+echo "Setup complete. You can now access the application at http://food2gether.local/"
+echo "Press Q to exit and remove the minikube cluster and dns resolver"
+do {
+  $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown").Character
+} while ($key -ne "q")
+
+minikube delete
+Remove-DnsClientNrptRule -Namespace "food2gether.local" -NameServer "$(minikube ip)"
--- bin/minikube-setup.sh
+++ bin/minikube-setup.sh
@@ -24,7 +24,33 @@ EOF
         ;;
     *)
         echo "Only MacOS is supported for now."
+        exit 1
         ;;
 esac
 
+if [ -n "$APPLICATION_COMPONENT" ]; then
+  flux suspend kustomization "$APPLICATION_COMPONENT" -n food2gether
+  kubectl delete -k "deployment/prod"
+  kubectl apply -k "deployment/local"
+fi
 
+echo "Setup complete. You can now access the application at http://food2gether.local/"
+echo "Press Q to exit and remove the minikube cluster and dns resolver"
+while true; do
+  read -n 1 -s -r; 
+  if [[ $REPLY =~ ^[Qq]$ ]]; then
+    break
+  fi
+done
+
+# Cleanup
+minikube delete
+case "$(uname -s)" in
+    Darwin*)
+        sudo rm -rf /etc/resolver/minikube-food2gether
+        ;;
+    *)
+        echo "Only MacOS is supported for now."
+        exit 1
+        ;;
+esac
