d007538a7aa2f247663e0131ff582d33fd4396f9
Marfien <contact@marfien.dev>
2024-11-26 13:02:52 +0100

    Removed admin privileges check

--- bin/minikube-setup.sh
+++ bin/minikube-setup.sh
@@ -8,11 +8,6 @@ FailGate() {
   fi
 }
 
-if [ "$EUID" -ne 0 ]; then
-  echo "You need to run this script with root privileges!"
-  exit 1
-fi
-
 # Install minikube & bootstrap flux
 
 # We are storing the curl result in a variable because otherwise the stdin would be closed and the eval would not work resulting in a "curl: (23) Failed writing body" error
@@ -22,19 +17,19 @@ eval "$minikube_setup_script"
 # Configure minikube dns
 case "$(uname -s)" in
     Darwin*)
-        sudo mkdir -p /etc/resolver
-        sudo cat <<EOF > /etc/resolver/minikube-food2gether
+      sudo mkdir -p /etc/resolver
+      sudo cat <<EOF > /etc/resolver/minikube-food2gether
 domain food2gether.local
 nameserver $(minikube ip)
 search_order 1
 timeout 5
 EOF
-        ;;
+      ;;
     *)
       # Note: the read command needs a different flag on linux: -k1 -> -n1
-        echo "Only MacOS is supported for now."
-        exit 1
-        ;;
+      echo "Only MacOS is supported for now."
+      exit 1
+      ;;
 esac
 
 if [ -n "$APPLICATION_COMPONENT" ]; then
@@ -47,7 +42,7 @@ fi
 echo "Setup complete. You can now access the application at http://food2gether.local/"
 echo "Press Q to exit and remove the minikube cluster and dns resolver"
 while true; do
-  read -srk1
+  read -srn1
   if [[ $REPLY =~ ^[Qq]$ ]]; then
     break
   fi
@@ -57,10 +52,10 @@ done
 minikube delete
 case "$(uname -s)" in
     Darwin*)
-        sudo rm -rf /etc/resolver/minikube-food2gether
-        ;;
+      sudo rm -rf /etc/resolver/minikube-food2gether
+      ;;
     *)
-        echo "Only MacOS is supported for now."
-        exit 1
-        ;;
+      echo "Only MacOS is supported for now."
+      exit 1
+      ;;
 esac
