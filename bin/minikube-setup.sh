35d27a681e444cd552eb2a0481add683987939b1
Marfien <contact@marfien.dev>
2024-11-28 12:15:15 +0100

    Add variable for local domain

--- bin/minikube-setup.sh
+++ bin/minikube-setup.sh
@@ -3,6 +3,8 @@
 # Fail on first error
 set -e
 
+LOCAL_DOMAIN="food2gether.local"
+
 github_pat=$1
 application_component=$2
 
@@ -21,7 +23,7 @@ fi
 minikube delete
 minikube start --addons storage-provisioner,ingress,ingress-dns
 
-flux bootstrap github --token-auth --owner=food2gether --repository=flux-base --branch=$(git rev-parse --abbrev-ref HEAD) --path=system <<< "$github_pat"
+flux bootstrap github --token-auth --owner=food2gether --repository=flux-base --branch=main --path=system <<< "$github_pat"
 echo "";
 echo "Cluster is set up!";
 echo "";
@@ -32,15 +34,15 @@ case "$(uname -s)" in
     Darwin*)
       sudo mkdir -p /etc/resolver
       cat <<EOF | sudo tee /etc/resolver/minikube-food2gether > /dev/null
-domain food2gether.local
+domain $LOCAL_DOMAIN
 nameserver $(minikube ip)
 search_order 1
 timeout 5
 EOF
       ;;
     MINGW64*)
-      powershell.exe -Command 'Get-DnsClientNrptRule | Where-Object { $_.Namespace -eq "food2gether.local" } | Remove-DnsClientNrptRule -Force'
-      powershell.exe -Command "Add-DnsClientNrptRule -Namespace food2gether.local -NameServers $(minikube ip)"
+      powershell.exe -Command "Get-DnsClientNrptRule | Where-Object { $_.Namespace -eq '$LOCAL_DOMAIN' } | Remove-DnsClientNrptRule -Force"
+      powershell.exe -Command "Add-DnsClientNrptRule -Namespace '$LOCAL_DOMAIN' -NameServers '$(minikube ip)'"
       ;;
     *)
       # Note: the read command needs a different flag on linux: -k1 -> -n1
@@ -58,7 +60,7 @@ fi
 
 clear
 echo ""
-echo "Setup complete. You can now access the application at http://food2gether.local/"
+echo "Setup complete. You can now access the application at http://$LOCAL_DOMAIN/"
 echo "Press Q to exit and remove the minikube cluster and dns resolver"
 while true; do
   read -srn1 REPLY < /dev/tty
@@ -75,7 +77,7 @@ case "$(uname -s)" in
       sudo rm -rf /etc/resolver/minikube-food2gether
       ;;
     MINGW64*)
-      powershell.exe -Command 'Remove-DnsClientNrptRule -Namespace "food2gether.local" -NameServer "'$(minikube ip)'"'
+      powershell.exe -Command "Remove-DnsClientNrptRule -Namespace '$LOCAL_DOMAIN' -NameServer '$(minikube ip)'"
       ;;
     *)
       echo "Unsupported OS."
